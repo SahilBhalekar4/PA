76369d99c0bfe54b2fd1d1ee1fb5128ab8c663e6
SahilBhalekar4 <166284149+SahilBhalekar4@users.noreply.github.com>
2025-04-28 22:44:54 +0530

    Transfer.cpp

--- EXTERNAL.cpp
+++ EXTERNAL.cpp
@@ -1,32 +1,25 @@
-#include<xc.h>
-#include<stdio.h>
-#include<stdlib.h>
-#include<pic18f4550.h>
- 
- void main (void)
- {
-    int source[]={0x1,0x2,0x3,0x4,0x5};
-    int dest[]={0x6,0x7,0x8,0x9,0xa};
-    int temp ,i,j,k;
-    for(i=0;i<5;i++)
-    {
-        temp=source[i];
-        source[i]=dest[i];
-        dest=[i]=temp[];
-    }
-     TRISB=0;
-     TRISD=0;
-
-     for(i=0;i<5;i++)
-     {
-        PORTB=dest[i];
-        PORTD=source[i];
-        for(j=0;j<1000;j++)
-        {
-            for(k=0;k<100;k++)
-            {
-            }
-        }
-     }
-     return;
- }
+#include <xc.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <pic18f4550.h>
+void main(void) {
+int i;
+int source[]={0x50,0x80,0x10,0x25,0x60};
+int dest[]={0x00,0x00,0x00,0x00,0x00};
+for(i=0;i<=4;i++)
+{
+dest[i]=source[i];
+}
+TRISD=0;
+for(i=0;i<=4;i++)
+{
+PORTD=dest[i];
+for(int n=0;n<1000;n++)
+{
+for(int j=0;j<500;j++)
+{
+}
+}
+}
+return;
+}
