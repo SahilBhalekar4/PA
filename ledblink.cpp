f80c8fe0358d5f204604e38c97617091a61a5621
SahilBhalekar4 <166284149+SahilBhalekar4@users.noreply.github.com>
2025-04-29 09:35:52 +0530

    ledblink.cpp

--- ledblink.cpp
+++ ledblink.cpp
@@ -1,10 +1,10 @@
 #include<xc.h>
 #include<pic18f4550.h>
 #include<stdlib.h>
-voidtodelay(void)
+void todelay(void)
 {
-T0CON=0x08;
-TMR0H=0x35;
+T0CON=0x03;
+TMR0H=0x02;
 TMR0L=0x00;
 T0CONbits.TMR0ON=1;
 while(INTCONbits.TMR0IF==0);
@@ -15,7 +15,7 @@ INTCONbits.TMR0IF=0;
 void main(void) {
 TRISD=0;
 while(1){
-PORTD=0x55;
+PORTD=0x10;
 todelay();
 PORTD=0x00;
 todelay();
